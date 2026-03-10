8bf2d48b4e30efa8daee47ed3bfe3341cab5fbbf
Joseber64 <luisenriqueramirez185@gmail.com>
2026-03-10 15:07:12 -0600

    Create TaskAdapter.kt

---
+++ app/app/src/main/java/com/quickearn/app/app/src/main/java/com/quickearn/app/TaskAdapter.kt
@@ -0,0 +1,28 @@
+package com.quickearn.app
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+class TaskAdapter(private val tasks: List<Task>) : RecyclerView.Adapter<TaskAdapter.TaskViewHolder>() {
+
+    class TaskViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val title: TextView = view.findViewById(R.id.taskTitle)
+        val reward: TextView = view.findViewById(R.id.taskReward)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TaskViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_task, parent, false)
+        return TaskViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: TaskViewHolder, position: Int) {
+        val task = tasks[position]
+        holder.title.text = task.title
+        holder.reward.text = "+$${task.reward} MXN"
+    }
+
+    override fun getItemCount() = tasks.size
+}
